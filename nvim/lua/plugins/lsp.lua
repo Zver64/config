ee4f4439a25ebea6ea61aa2c55f359cec90e8a5b
Zver64 <coolvillerxxx@gmail.com>
2026-06-30 18:00:23 +0700

    better lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,11 +1,22 @@
 return {
   "neovim/nvim-lspconfig",
+  init = function()
+    vim.lsp.enable("copilot")
+  end,
   opts = {
     servers = {
       dartls = {
         settings = {
           dart = {
             includeDependenciesInWorkspaceSymbols = false,
+          },
+        },
+      },
+      eslint = {
+        settings = {
+          validate = "probe",
+          workingDirectory = {
+            mode = "auto",
           },
         },
       },
